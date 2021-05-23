8c6a69c06ae719b2400ecd6d8e581822ecac5beb
Roman <68850770+Roman27011986@users.noreply.github.com>
2021-05-23 23:30:36 +0300

    fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -8,7 +8,7 @@ import style from './ContactForm.module.css';
 
 //material.ui import
 
-import TextField from '@material-ui/core/TextField';
+
 import Button from "@material-ui/core/Button";
 import Menu from '@material-ui/core/Menu';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
